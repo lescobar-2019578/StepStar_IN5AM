1b7e14ca53c1fafc053c09d74e3151d430b27ca4
dzabala-2019001 <dzabala-2019001@kinal.edu.gt>
2023-08-16 18:51:26 -0600

    subo mi vista de Empleados y modifico el controlador y principal v2

--- StepStar_IN5AM/src/java/modelo/EmpleadosDAO.java
+++ StepStar_IN5AM/src/java/modelo/EmpleadosDAO.java
@@ -16,6 +16,32 @@ public class EmpleadosDAO {
    ResultSet rs;
    int resp;
    
+   public Empleados Validar(String nombresEmpleado, String telefonoContacto){
+       
+        Empleados empleados = new Empleados();
+        
+        String sql = "select *  from empleados where nombresEmpleado = ? and DPI = ?";
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, nombresEmpleado);
+            ps.setString(2, telefonoContacto);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                empleados.setCodigoEmpleado(rs.getInt("codigoEmpleado"));
+                empleados.setDPI(rs.getString("DPI"));
+                empleados.setApellidosEmpleado(rs.getString("apellidosEmpleado"));
+                empleados.setNombresEmpleado(rs.getString("nombresEmpleado"));
+                empleados.setTelefonoContacto(rs.getString("telefonoContacto"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+        return empleados; 
+    }
+   
    //METODO LISTAR
    public List listar(){
        String sql = "select * from Empleados";
@@ -27,11 +53,13 @@ public class EmpleadosDAO {
            while(rs.next()){
                Empleados em = new Empleados();
                em.setCodigoEmpleado(rs.getInt(1));
-               em.setApellidosEmpleado(rs.getString(2));
-               em.setNombresEmpleado(rs.getString(3));
-               em.setDireccionEmpleado(rs.getString(4));
-               em.setTelefonoContacto(rs.getString(5));
-               em.setCodigoTipoEmpleado(rs.getInt(6));
+               em.setDPI(rs.getString(2));
+               em.setApellidosEmpleado(rs.getString(3));
+               em.setNombresEmpleado(rs.getString(4));
+               em.setDireccionEmpleado(rs.getString(5));
+               em.setTelefonoContacto(rs.getString(6));
+               em.setCodigoTipoEmpleado(rs.getInt(7));
+               listaEmpleados.add(em);
            }
        }catch(Exception e){
            e.printStackTrace();
@@ -46,11 +74,13 @@ public class EmpleadosDAO {
        try{
            con = cn.Conexion();
            ps = con.prepareStatement(sql);
-           ps.setString(1, em.getApellidosEmpleado());
-           ps.setString(2, em.getNombresEmpleado());
-           ps.setString(3, em.getDireccionEmpleado());
-           ps.setString(4, em.getTelefonoContacto());
-           ps.setInt(5, em.getCodigoTipoEmpleado());
+           
+           ps.setString(1, em.getDPI());
+           ps.setString(2, em.getApellidosEmpleado());
+           ps.setString(3, em.getNombresEmpleado());
+           ps.setString(4, em.getDireccionEmpleado());
+           ps.setString(5, em.getTelefonoContacto());
+           ps.setInt(6, em.getCodigoTipoEmpleado());
            ps.executeUpdate();
        } catch (Exception e) {
            e.printStackTrace();
@@ -58,7 +88,7 @@ public class EmpleadosDAO {
        return resp;
     }   
    
-   //METODOD BUSCAR
+   //METODO BUSCAR
    
    public Empleados listarCodigoPredocutos(int id){
        Empleados em = new Empleados();
@@ -68,11 +98,12 @@ public class EmpleadosDAO {
            ps = con.prepareStatement(sql);
            rs = ps.executeQuery();
            while(rs.next()){
-               em.setApellidosEmpleado(rs.getString(2));
-               em.setNombresEmpleado(rs.getString(3));
-               em.setDireccionEmpleado(rs.getString(4));
-               em.setTelefonoContacto(rs.getString(5));
-               em.setCodigoTipoEmpleado(rs.getInt(6));
+               em.setDPI(rs.getString(2));
+               em.setApellidosEmpleado(rs.getString(3));
+               em.setNombresEmpleado(rs.getString(4));
+               em.setDireccionEmpleado(rs.getString(5));
+               em.setTelefonoContacto(rs.getString(6));
+               em.setCodigoTipoEmpleado(rs.getInt(7));
            }
        }catch(Exception e){
            e.printStackTrace();
@@ -81,16 +112,17 @@ public class EmpleadosDAO {
        return em;
    }
   
-   //METODO EDIATAR
+   //METODO EDITAR
    public int actualizar(Empleados em){
-       String sql = "upadate Empleados set apellidosEmpleado = ?, nombresEmpleado = ?, direccionEmpleado = ?, telefonoContacto = ?" + "where codigoEmpleado = ?";
+       String sql = "update Empleados set DPI = ?, apellidosEmpleado = ?, nombresEmpleado = ?, direccionEmpleado = ?, telefonoContacto = ?" + "where codigoEmpleado = ?";
        try{
           con = cn.Conexion();
           ps = con.prepareStatement(sql);
-          ps.setString(1, em.getApellidosEmpleado());
-          ps.setString(2, em.getNombresEmpleado());
-          ps.setString(3, em.getDireccionEmpleado());
-          ps.setString(4, em.getTelefonoContacto());
+          ps.setString(1, em.getDPI());
+          ps.setString(2, em.getApellidosEmpleado());
+          ps.setString(3, em.getNombresEmpleado());
+          ps.setString(4, em.getDireccionEmpleado());
+          ps.setString(5, em.getTelefonoContacto());
           ps.executeUpdate();
        }catch(Exception e){
            e.printStackTrace();
