9c5dfa38645b3683c325aacb5b6f5531649b61b5
lescobar-2019578 <lescobar-2019578@kinal.edu.gt>
2023-08-16 22:19:37 -0600

    modifique mi archivo, por errores

--- DBStepStarIN5AM.sql
+++ DBStepStarIN5AM.sql
@@ -1,6 +1,11 @@
 /*
-	Fecha de creacion: 05/07/2023
-    Fecha de modificacion: 07/07/2023
+	Fecha de creacion: 
+		05/07/2023
+    
+    Fecha de modificacion: 
+		07/07/2023,
+		14/08/2023
+        
     Programadores:
 		Axel  Antonio Xitumul Chén - 2022038
         Diego Estuardo Zabala Toc - 2019001
@@ -15,12 +20,13 @@ Use DBStepStarIN5AM;
 
 Create table TipoEmpleado(
 	codigoTipoEmpleado int not null auto_increment,
-    descripcion varchar (50) not null,
+    descTipoEmpleado varchar (50) not null,
     sueldo Decimal(10,2) not null,
     bonificacion Decimal(10,2) not null,
     categoriaEmpleado varchar(150) not null,
     primary key PK_codigoTipoEmpleado (codigoTipoEmpleado)
 );
+
 
 Create table Cliente(
 	codigoCliente int not null auto_increment,
@@ -32,15 +38,6 @@ Create table Cliente(
     primary key PK_codigoCliente (codigoCliente)
 );
 
-Create table Usuario(
-	codigoUsuario int not null auto_increment,
-    nombreUsuario varchar (100) not null,
-    apellidoUsuario varchar (100) not null,
-    usuarioLogin varchar (50) not null,
-    contrasena varchar (50) not null,
-    primary key PK_CodigoUsuario (codigoUsuario)
-);
-
 Create table login(
 	usuario varchar (50),
     passwordLogin varchar (50),
@@ -50,11 +47,12 @@ Create table login(
 Create Table Categoria(
 	codigoCategoria int not null auto_increment,
     nombreCategotia varchar(150) not null,
-    descripcion varchar(150) not null,
+    descCategoria varchar(150) not null,
     genero boolean not null,
     codigoProducto int not null,
     primary key PK_codigoCategoria (codigoCategoria)
 );
+
 
 create table Proveedores(
 	codigoProveedor int not null auto_increment,
@@ -68,7 +66,7 @@ create table Proveedores(
 Create table Productos(
 	codigoProducto int not null auto_increment,
 	nombreProducto varchar (150) not null,
-    descripcion varchar(150) not null,
+    descProductos varchar(150) not null,
     marca varchar(150) not null,
     precio Decimal(10,2) not null,
     talla int not null,
@@ -84,6 +82,7 @@ Create table Productos(
 
 Create table Empleados(
 	codigoEmpleado int not null auto_increment,
+    DPI varchar(20) not null,
     apellidosEmpleado varchar (150) not null,
     nombresEmpleado varchar (150) not null,
     direccionEmpleado varchar (150) not null,
@@ -110,7 +109,7 @@ Create table Servicios (
 Create table Venta(
 	codigoVenta int not null auto_increment,
     fechaVenta date not null,
-    descripcion varchar(150) not null,
+    descVenta varchar(150) not null,
     cantidad decimal(10,2) not null,
     direccionEnvio varchar(150) not null,
     codigoServicio int not null,
@@ -122,7 +121,7 @@ Create table Venta(
 Create table Compra(
 	codigoCompra int not null auto_increment,
     fechaCompra Date not null,
-    descripcion varchar(150) not null,
+    descCompra varchar(150) not null,
     cantidad decimal(10,2) not null,
     codigoProducto int not null,
     primary key PK_codigoCompra (codigoCompra),
@@ -154,3 +153,109 @@ Create table DetalleVenta(
 	constraint FK_DetalleVenta_Productos foreign key
 		(codigoProducto) references Productos (codigoProducto)
 );
+
+
+-- TipoEmpleado:
+
+insert into TipoEmpleado(codigoTipoEmpleado, descTipoEmpleado, sueldo, bonificacion, categoriaEmpleado)
+	values(1,'vende zapatos de ADIDAS',3500.00,250.00,'Vendedor');
+insert into TipoEmpleado(codigoTipoEmpleado, descTipoEmpleado, sueldo, bonificacion, categoriaEmpleado)
+	values(2,'cobro de productos en caja',3500.00,250.00,'Cajero');
+insert into TipoEmpleado(codigoTipoEmpleado, descTipoEmpleado, sueldo, bonificacion, categoriaEmpleado)
+	values(3,'limpieza de la empresa',3500.00,250.00,'Conserje');    
+    
+-- Cliente:
+
+insert into Cliente(codigoCliente, nombreCliente, apellidoCliente, nit, estadoActivo, telefonoCliente)
+	values(1,'Diego','Zabala','601278-7',1,'42464357');
+insert into Cliente(codigoCliente, nombreCliente, apellidoCliente, nit, estadoActivo, telefonoCliente)
+	values(2,'David','Godoy','247516-7',1,'37181157');
+insert into Cliente(codigoCliente, nombreCliente, apellidoCliente, nit, estadoActivo, telefonoCliente)
+	values(3,'Axel','Xitumul','121456-2',1,'24741959');
+
+-- Categoria:
+
+insert into Categoria(codigoCategoria, nombreCategotia, descCategoria, genero, codigoProducto)
+	values(1,'Zapato Formal','Zapato de vestir',1,1);
+insert into Categoria(codigoCategoria, nombreCategotia, descCategoria, genero, codigoProducto)
+	values(2,'Casual','Zapato informal',0,2);
+insert into Categoria(codigoCategoria, nombreCategotia, descCategoria, genero, codigoProducto)
+	values(3,'Deportivo','Zapato para deporte',1,3);
+    
+-- Proveedores:
+
+insert into Proveedores(codigoProveedor, nombreProveedor, direccionProveedor, telefonoProveedor, correoProveedor)  
+	values(1,'ADIDAS','Zona 7 Tikal 1','24741959','Adidas@Yahoo.com');
+insert into Proveedores(codigoProveedor, nombreProveedor, direccionProveedor, telefonoProveedor, correoProveedor)  
+	values(2,'NIKE','Zona 10 De Mixco, Guatemala','12405896','Nike@gmail.com');
+insert into Proveedores(codigoProveedor, nombreProveedor, direccionProveedor, telefonoProveedor, correoProveedor)  
+	values(3,'Flexi','C.C. Miraflores, 2do Nivel, Local 255','24741959','Flexi@Yahoo.com');
+  
+-- Productos:
+
+insert into Productos(codigoProducto, nombreProducto, descProductos, marca, precio, talla, cantidad, codigoProveedor, codigoCategoria)
+	values(1,'STAN SMITH SHOES ', 'Color verde con cuerdas extra','ADIDAS',879.99,10,10,1,1);
+insert into Productos(codigoProducto, nombreProducto, descProductos, marca, precio, talla, cantidad, codigoProveedor, codigoCategoria)
+	values(2,'AIR JORDAN 1 RETRO HI OG RMSTD HOMBRE DZ5485-051 ', 'Color Gris con cuerdas extra','JORDAN',2199.00,10,5,2,2);
+insert into Productos(codigoProducto, nombreProducto, descProductos, marca, precio, talla, cantidad, codigoProveedor, codigoCategoria)
+	values(3,'RACER TR23 SHOES', 'Color gris con un par de cuerdas extra','ADIDAS',550.00,8,50,1,3);
+    
+-- Empleados:
+
+insert into Empleados(codigoEmpleado, DPI, apellidosEmpleado, nombresEmpleado, direccionEmpleado, telefonoContacto, codigoTipoEmpleado)
+    values(1,'123456789123' ,'Garcia','Carlos','Zona 7, Tikal 1 30-07', '24741959',1 );
+insert into Empleados(codigoEmpleado,  DPI, apellidosEmpleado, nombresEmpleado, direccionEmpleado, telefonoContacto, codigoTipoEmpleado)
+    values(2,'358812220101' ,'Perez','Jose', 'Zona 1, Mixco', '36589632', 3 );
+insert into Empleados(codigoEmpleado,  DPI, apellidosEmpleado, nombresEmpleado, direccionEmpleado, telefonoContacto, codigoTipoEmpleado)
+    values(3, '654622430101','Litera','Sandra','Ciudad De Plata ', '42464357',2);
+ 
+-- Servicios:
+
+insert into Servicios(codigoServicio, fechaServicio, tipoServicio, horaServicio, lugarServicio, telefonoServicio, codigoEmpleado)
+	values(1,'2023-03-30','Domicilio','13:00:00','Zona 7 Tikal 1 30-07','28356985',1);
+insert into Servicios(codigoServicio, fechaServicio, tipoServicio, horaServicio, lugarServicio, telefonoServicio, codigoEmpleado)
+	values(2,'2022-04-22','Domicilio','18:00:00','MAJADAS','37181157',1);
+insert into Servicios(codigoServicio, fechaServicio, tipoServicio, horaServicio, lugarServicio, telefonoServicio, codigoEmpleado)
+	values(3,'2023-03-15','Domicilio','08:00:00','Ciudad De Plata','19592474',1);
+    
+-- Venta:
+
+insert into Venta(codigoVenta, fechaVenta, descVenta, cantidad, direccionEnvio, codigoServicio)
+	values(1,'2023-03-30','Compra de Jordan 1',2,'Zona 7 Tikal 1 30-07',1);
+insert into Venta(codigoVenta, fechaVenta, descVenta, cantidad, direccionEnvio, codigoServicio)
+	values(2,'2023-03-30','Compra de zapato deportivo',1,'2022-04-22',2);
+insert into Venta(codigoVenta, fechaVenta, descVenta, cantidad, direccionEnvio, codigoServicio)
+	values(3,'2023-03-30','Compra de Zapato formal',1,'2023-03-15',3);
+    
+-- Compra:
+
+insert into Compra(codigoCompra, fechaCompra, descCompra, cantidad, codigoProducto)
+	values(1,'2023-03-30','Compra de Jordan 1',2,1);
+insert into Compra(codigoCompra, fechaCompra, descCompra, cantidad, codigoProducto)
+	values(2,'2023-03-30','Compra de zapatos deportivos',4,3);
+insert into Compra(codigoCompra, fechaCompra, descCompra, cantidad, codigoProducto)
+	values(3,'2023-03-30','Compra de Zapato formal',1,2);
+    
+-- DetalleCompra:
+
+insert into DetalleCompra(codigoDetalleCompra, codigoVenta, codigoCompra)
+	values(1,1,1);
+insert into DetalleCompra(codigoDetalleCompra, codigoVenta, codigoCompra)
+	values(2,2,2);
+insert into DetalleCompra(codigoDetalleCompra, codigoVenta, codigoCompra)
+	values(3,3,3);
+    
+-- DetalleVenta
+
+insert into DetalleVenta(codigoDetalleVenta, codigoCliente, codigoVenta, codigoProducto)
+	values(1,2,1,1);
+insert into DetalleVenta(codigoDetalleVenta, codigoCliente, codigoVenta, codigoProducto)
+	values(2,1,2,2);
+insert into DetalleVenta(codigoDetalleVenta, codigoCliente, codigoVenta, codigoProducto)
+	values(3,3,3,3);
+
+
+
+
+    
+    
